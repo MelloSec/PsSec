854e8af1b66eee1d26ee7c879e02b42c8587fd75
mellonaut <65114647+mellonaut@users.noreply.github.com>
2022-07-06 18:55:10 -0400

    Parameter binding for nmap module

--- PsSec.ps1
+++ PsSec.ps1
@@ -1,17 +1,41 @@
 function os-scan {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$ip
+    )
     nmap -O --osscan-guess -sV -vv -oN _nmap_os_scan $ip
 }
 
 function sv-scan {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$ip
+    )    
     nmap -sV -sC -A -vv -oN _nmap_tcp_sv $ip
 }
 
+function sup-scan {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$ip
+    )
+    nmap -sN -oN _nmap_sup $ip
+}
+
 function psv-scan {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$ip
+    )
     nmap -Pn -p- -sV -sC -A -vv -oN _nmap_psv $ip
 }
 
 
 function tcp-scan {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$ip
+    )
     $scan1 = nmap -sV -sC -A -Pn -vv $ip
     $scan2 = nmap -sV -sC -Pn -p- -T4 -vv $ip
     $scans = $scan1,$scan2
@@ -20,29 +44,57 @@ function tcp-scan {
 }
 
 function vulners-scan {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$ip
+    )
     nmap -Pn -sV -vv --script vulners --script-args mincvss=5.0 -oN _nmap_vulners $ip
 }
 
 function old-tcp {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$ip
+    )
     nmap -sV -sC -A -vv -oN _nmap_tcp_asc $ip
     nmap -sV -sC -p- -T4 -vv -oN _nmap_tcp_p $ip
 }
 
 function su-scan {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$ip
+    )
     nmap -sU -Pn -vv --top-ports 1000 -oN _nmap_su $ip
 }
 
 function psu-scan {
-    nmap -sU -p- -Pn -vv --top-ports 1000 -oN _nmap_psu $ip
+    nmap -sU -p- -Pn -
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$ip
+    )vv --top-ports 1000 -oN _nmap_psu $ip
 }
 
 function suv-scan {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$ip
+    )
     nmap -sUV -Pn -vv --top-ports 1000 -oN _nmap_suv $ip
 }
 function psuv-scan {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$ip
+    )
     nmap -sUV -Pn -p- -vv -T4 --max-retries 0 --top-ports 1000 -oN _nmap_psuv $ip
 }
 function udp-scan {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$ip
+    )
     $scan1 = nmap -sU -Pn -vv --top-ports 1000 $ip
     $scan2 = nmap -sU -Pn vv -T4 --max-retries 0 $ip
     $scans = $scan1,$scan2
@@ -51,63 +103,119 @@ function udp-scan {
 }
 
 function old-udp {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$ip
+    )
     nmap -sU -vv --top-ports 1000 -oN _nmap_udp_1000 $ip
     nmap -sU --Pn vv -T4 --max-retries 0 -oN _nmap_udp_2 $ip
 }
 
 
 function long-scan {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$ip
+    )
     nmap -sC -sV -A -p- -T4 -vv -oN _nmap_tcp_full $ip
     nmap -sU --top-ports 5000 -oN sudo_nmap_udp_5000 $ip
 }
 
 function smb-quick {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$ip
+    )
     nmap -vv --script=smb-os-discovery,smb-enum-shares,smb-enum-users -oN _nmap_smb_quick $ip
 }
 
 function smb-vuln {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$ip
+    )
     nmap -p 139,445 -vv --script=smb-vuln-cve2009-3103,smb-vuln-ms06-025,smb-vuln-ms07-029,smb-vuln-ms08-067,smb-vuln-ms10-054,smb-vuln-ms10-061,smb-vuln-ms17-010 -oN _nmap_smb_cve $ip
     nmap -p 139,445 -vv -T4 -oN _nmap_smb_vulns -Pn --script 'not brute and not dos and smb-*' -vv -d $ip
 }
 
 function smb-users {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$ip
+    )
     nmap -vv --script smb-enum-users.nse -p445 -oN _nmap_smbusers $ip
 }
 
 function smb-shares {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$ip
+    )
     nmap -vv --script=smb-enum-shares -p445 -oN _nmap_smb_shares $ip
 }
 
 function smb-sessions {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$ip
+    )
     nmap -vv --script=smb-enum-sessions -p445 -oN _nmap_smb_sessions $ip
 }
 
 function nfs-scan{
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$ip
+    )
     nmap -v -p 111 --script=rpcinfo,nfs* -oN _nmap_nfs $ip
 }
 
 function enum-shares {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$ip
+    )
     nmap -vv --script=smb-brute,smb-enum-shares,smb-enum-users,smb-enum-sessions -p445 -oN _nmap_enum_shares $ip
     nmap -vv --script=smb-brute,smb-enum-shares,smb-enum-users,smb-enum-sessions --script-args smbuser=username,smbpass=password -p445 -oN _nmap_enum_shares2 $ip 
 }
  
 
 function enum-snmp {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$ip
+    )
     nmap -sU --open -p 161 -oN _nmap_open_snmp $ip
 }
 
 function subnet-scan {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$ip
+    )
     nmap -T5 -n -sn -oN _nmap_subnet $ip 
 }
 function 5000-scan {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$ip
+    )
     nmap -Pn -A -p 1-5000 -vvv -oN _nmap_5000 $ip
 }
 
 function ping-sweep {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$ip
+    )
     nmap -sP -oN _nmap_pingsweep $ip
 }
 
 function force-scan {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$ip
+    )
     nmap -vvv -Pn -p- -sV -sC -A -oN _nmap_force $ip
 }
 function nbt-scan {
@@ -320,17 +428,17 @@ function mssql-brute {
 }
 # need a vanilla
 function web-go {
-    gobuster dir http://$ip -w $word -o goscan.txt
+    gobuster dir -u http://$ip -w $word -o goscan.txt
 }
 
 # gobuster has vhost mode
 function web-vhost {
-    gobuster vhost http://$ip -w $word -o govhost.txt
+    gobuster -v vhost -u http://$ip -w $word -o govhost.txt
 }
 
 # gobuster has dns mode
 function web-dns {
-    gobuster dns http://$ip -w $word -o godns.txt
+    gobuster -v dns http://$ip -w $word -o godns.txt
 }
 
 # a php specific that will use both go and ferox
