711c7630453f19331070592256a7ccfe59171490
mellonaut <65114647+mellonaut@users.noreply.github.com>
2022-07-06 18:37:16 -0400

    New modules, new placeholders, too

---
+++ Modules/ftp-telnet/ftp-telnet.psm1
@@ -0,0 +1,1 @@
+# Placeholder for older file server types
--- PsSec.ps1
+++ PsSec.ps1
@@ -318,3 +318,30 @@ function mssql-scan {
 function mssql-brute {
     nmap -vv -p 3306 -Pn --script ms-sql-brute --script-args userdb=$userlist,passdb=$passlist $ip
 }
+# need a vanilla
+function web-go {
+    gobuster dir http://$ip -w $word -o goscan.txt
+}
+
+# gobuster has vhost mode
+function web-vhost {
+    gobuster vhost http://$ip -w $word -o govhost.txt
+}
+
+# gobuster has dns mode
+function web-dns {
+    gobuster dns http://$ip -w $word -o godns.txt
+}
+
+# a php specific that will use both go and ferox
+function web-php {
+    gobuster dir http://$ip -w $word -o gophp.txt -x php,php3,php5,html
+    feroxbuster 
+}
+
+# # try and get something to work well with zap
+# func zap-scan
+
+# # nikto scan
+# func nikto-scan
+
