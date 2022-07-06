72af2f1f196fb4341b64c28d86d694c5f01ab75e
mellonaut <65114647+mellonaut@users.noreply.github.com>
2022-07-06 03:47:44 -0400

    Added module for mssql

---
+++ Modules/mssql-module/mssql.psm1
@@ -0,0 +1,7 @@
+function mssql-scan {
+    nmap -sV -p 3306 --script mysql-audit,mysql-databases,mysql-dump-hashes,mysql-empty-password,mysql-enum,mysql-info,mysql-query,mysql-users,mysql-variables,mysql-vuln-cve2012-2122 -oN _nmap_mssql-scan $ip
+}
+
+function mssql-brute {
+    nmap -vv -p 3306 -Pn --script ms-sql-brute --script-args userdb=$userlist,passdb=$passlist $ip
+}
