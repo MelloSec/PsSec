892619be3765bf93b5c37fdabeeaf783ddeefb3e
mellonaut <65114647+mellonaut@users.noreply.github.com>
2022-07-09 21:12:29 -0400

    more testssl modules

--- Modules/docker/docker.psm1
+++ Modules/docker/docker.psm1
@@ -1,25 +1,32 @@
 $domain = "google.com"
 $uri = "https://$domain"
 
-function testssl-e {
-    docker run --rm -ti drwetter/testssl.sh:3.0 -e --parallel $uri
-}
-
-function testssl-ef {
-    docker run --rm -ti drwetter/testssl.sh:3.0 -e --fast --parallel $uri
+function testssl {
+    docker run --rm -ti drwetter/testssl.sh:3.0 $uri
 }
 
 function testssl-p {
-    docker run --rm -ti drwetter/testssl.sh:3.0 -p --fast --parallel $uri
+    docker run --rm -ti drwetter/testssl.sh:3.0 --parallel $uri
 }
-function testssl-pf {
-    docker run --rm -ti drwetter/testssl.sh:3.0 -p --fast --parallel $uri
+function testssl-e {
+    docker run --rm -ti drwetter/testssl.sh:3.0 -e $uri
+}
+
+function testssl-epf {
+    docker run --rm -ti drwetter/testssl.sh:3.0 -e --parallel --fast  $uri
+}
+
+function testssl-p {
+    docker run --rm -ti drwetter/testssl.sh:3.0 -p $uri
+}
+function testssl-ppf {
+    docker run --rm -ti drwetter/testssl.sh:3.0 -p --parallel --fast  $uri
 }
 
 function testssl-t {
-    docker run --rm -ti drwetter/testssl.sh:3.0 -t --parallel $uri
+    docker run --rm -ti drwetter/testssl.sh:3.0 -t $uri
 }
 
-function testssl-tf {
-    docker run --rm -ti drwetter/testssl.sh:3.0 -t --fast --parallel $uri
+function testssl-tpf {
+    docker run --rm -ti drwetter/testssl.sh:3.0 -t --parallel --fast  $uri
 }
