179c71834c38fbf8ccdff6b7d9fd4ee9c7aa7d37
mellonaut <65114647+mellonaut@users.noreply.github.com>
2022-07-09 21:37:36 -0400

    parameter binding

--- PsSec.psm1
+++ PsSec.psm1
@@ -559,25 +559,64 @@ function web-php {
 $domain = "google.com"
 $uri = "https://$domain"
 
-function testssl-e {
-    docker run --rm -ti drwetter/testssl.sh:3.0 -e --parallel $uri
-}
-
-function testssl-ef {
-    docker run --rm -ti drwetter/testssl.sh:3.0 -e --fast --parallel $uri
+function testssl {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$uri
+    )
+    docker run --rm -ti drwetter/testssl.sh:3.0 $uri
 }
 
 function testssl-p {
-    docker run --rm -ti drwetter/testssl.sh:3.0 -p --fast --parallel $uri
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$uri
+    )
+    docker run --rm -ti drwetter/testssl.sh:3.0 --parallel $uri
 }
-function testssl-pf {
-    docker run --rm -ti drwetter/testssl.sh:3.0 -p --fast --parallel $uri
+function testssl-e {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$uri
+    )
+    docker run --rm -ti drwetter/testssl.sh:3.0 -e $uri
+}
+
+function testssl-epf {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$uri
+    )
+    docker run --rm -ti drwetter/testssl.sh:3.0 -e --parallel --fast  $uri
+}
+
+function testssl-p {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$uri
+    )
+    docker run --rm -ti drwetter/testssl.sh:3.0 -p $uri
+}
+function testssl-ppf {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$uri
+    )
+    docker run --rm -ti drwetter/testssl.sh:3.0 -p --parallel --fast  $uri
 }
 
 function testssl-t {
-    docker run --rm -ti drwetter/testssl.sh:3.0 -t --parallel $uri
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$uri
+    )
+    docker run --rm -ti drwetter/testssl.sh:3.0 -t $uri
 }
 
-function testssl-tf {
-    docker run --rm -ti drwetter/testssl.sh:3.0 -t --fast --parallel $uri
+function testssl-tpf {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$uri
+    )
+    docker run --rm -ti drwetter/testssl.sh:3.0 -t --parallel --fast  $uri
 }
